36ba675b5ea8a15b8918b819dab1b0d81ff3d4c4
Ilya Zverkov <izverk78@yandex.ru>
2021-09-13 18:11:50 +0300

    реализована интерактивность

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,44 @@
-//объекты для открытия и закрытия popup
-const popup = document.querySelector('.popup');
-const popupOpenBtn = document.querySelector('.edit-button');
-const popupCloseBtn = popup.querySelector('.popup__close');
+// Выбор объектов для открытия и закрытия popup
+const popup = document.querySelector(".popup");
+const popupOpenBtn = document.querySelector(".edit-button");
+const popupCloseBtn = popup.querySelector(".popup__close");
 
-//объекты для вставки плейсхолдеров в поля формы
-let nameInput = popup.querySelector('.popup__input_type_name');
-let jobInput = popup.querySelector('.popup__input_type_job');
-let nameProfile = document.querySelector('.profile__name-text');
-let jobProfile = document.querySelector('.profile__job');
+// Выбор объектов для вставки плейсхолдеров в поля формы
+// и перезаписи полей профиля данными из формы
+let nameInput = popup.querySelector(".popup__input_type_name");
+let jobInput = popup.querySelector(".popup__input_type_job");
+let form = popup.querySelector(".popup__container");
+let nameProfile = document.querySelector(".profile__name-text");
+let jobProfile = document.querySelector(".profile__job");
 
-
-
-//функция открытия-закрытия popup
+// Функция открытия-закрытия popup
 function popupToggle() {
-  popup.classList.toggle('popup_opened');
+  popup.classList.toggle("popup_opened");
 }
-//функции вставки плейсхолдеров в поля формы
+
+// Функции вставки плейсхолдеров в поля формы
 function insertNamePlaceholder() {
-  nameInput.setAttribute('placeholder', nameProfile.textContent);
+  nameInput.setAttribute("placeholder", nameProfile.textContent);
 }
 function insertJobPlaceholder() {
-  jobInput.setAttribute('placeholder', jobProfile.textContent);
+  jobInput.setAttribute("placeholder", jobProfile.textContent);
 }
 
-//открываем-закрываем popup
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
+// Обработчик события отправки формы при нажатии на кнопку "Сохранить"
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // отмена стандартной отправки формы
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  popupToggle();
+}
 
-//вставляем плейсхолдеры в поля формы
+// Отслеживание кликов для открытия/закрытия popup
+popupOpenBtn.addEventListener("click", popupToggle);
+popupCloseBtn.addEventListener("click", popupToggle);
+
+// Вставляем плейсхолдеры в поля формы
 insertNamePlaceholder();
 insertJobPlaceholder();
 
+//перезаписываем поля профиля данными из формы
+form.addEventListener("submit", formSubmitHandler);
