86eb661e69f5ae9296eaf77268ed42962cf02c29
Ilya Zverkov <izverk78@yandex.ru>
2021-09-26 18:49:13 +0300

    feat: реализовать функцию создания карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
-// Попап и кнопки его открытия/закрытия
-const popup = document.querySelector(".popup");
-const popupOpenBtn = document.querySelector(".profile__edit-button");
-const popupCloseBtn = popup.querySelector(".popup__close-button");
-// Форма и её поля ввода данных
-let form = popup.querySelector(".popup__form");
-let nameInput = popup.querySelector(".popup__input_type_name");
-let jobInput = popup.querySelector(".popup__input_type_job");
-// Поля профиля пользователя
+// Берем элемент попап и кнопки его открытия/закрытия
+const popup = document.querySelector('.popup');
+const popupOpenBtn = document.querySelector('.profile__edit-button');
+const popupCloseBtn = popup.querySelector('.popup__close-button');
+// Берем форму редактирования профиля и её поля ввода данных
+let form = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_job');
+// Берем поля профиля пользователя
 let nameProfile = document.querySelector(".profile__name");
 let jobProfile = document.querySelector(".profile__job");
-
-//Массив карточек с их именами и фотографиями
-const initialCards = [
+//Берем контейнер галереи для дальнейшей вставки в него карточек
+const galleryList = document.querySelector('.gallery__list');
+//Берем шаблон для создания карточки
+const cardTemplate = document.querySelector('.card-template');
+//Определяем массив карточек с их именами и путями к картинкам
+//для первоначального заполнения галереи при загрузке страницы
+let initialCards = [
   {
     name: 'Елизово. Корякский вулкан',
     link: './images/koryaksky_volcano_elizovo.jpg'
@@ -38,6 +42,14 @@ const initialCards = [
   }
 ];
 
+
+//Создание карточки
+function createCard(initialCards) {
+  const newCard = cardTemplate.content.cloneNode(true);
+  newCard.querySelector('.card__name').textContent = initialCards.name;
+  newCard.querySelector('.card__photo').setAttribute('src', initialCards.link);
+  galleryList.append(newCard);
+}
 // Открытие попапа
 function popupOpen() {
   popup.classList.add("popup_opened");
@@ -56,6 +68,11 @@ function formSubmitHandler(evt) {
   popupClose();
 }
 
+
+//Заполняем галерею карточками
+initialCards.forEach(createCard);
+// console.log(card.querySelector('.card__photo'))
+// console.dir(card.querySelector('.card__photo'))
 // Отслеживаем клики на кнопках открытия/закрытия попапа
 popupOpenBtn.addEventListener("click", popupOpen);
 popupCloseBtn.addEventListener("click", popupClose);
